f91e953c562e7db03cfe51e1fa697b640af94f52
lucas11-ho <lacus.mm.ph@gmail.com>
2026-02-08 12:05:55 +0630

    Update

--- frontend/src/ui/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-// Thin re-export wrapper to keep imports stable.
-// Some files import Button from "../ui/components/Button" while the base
-// component lives in Buttons.jsx.
-
-export { Button } from "./Buttons.jsx";
